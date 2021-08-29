1a9eeec62ddba6c141d26262f031e776e67e783b
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2021-08-29 13:06:37 -0300

    adding classes and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[300],
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            print('I got pressed');
+          });
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
